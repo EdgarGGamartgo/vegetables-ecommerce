2ff91bf6f536f27176ff9e958925a1ddd199795e
EdgarGGamartgo <edgarggamartgo@gmail.com>
2021-01-05 20:42:07 -0600

    Fix downloads

--- src/pages/Inventario.js
+++ src/pages/Inventario.js
@@ -2,30 +2,125 @@ import readXlsxFile from 'read-excel-file'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import CsvDownload from 'react-json-to-csv'
+import { Modal } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+
+  const useStylesModal = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      justify: 'center',
+      left: '50%',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[1],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
 
 const Inventario = () => {
-
+    const classesModal = useStylesModal();
     const [uploadedFile, setUploadedFile] = useState('')
     const [inventario, setInventario] = useState([])
     const [mockData, setMockData] = useState([])
+    const [modal, handleModal] = useState(false)
+    const [modalStyle] = useState(getModalStyle);
+    const [modalContent, setModalContent] = useState({
+        title: 'Contraseña',
+        content: 'Por favor ingrese contraseña',
+        ok: true
+    })
+    const [password, setPassword] = useState('')
+    
+    const openModal = () => {
+        handleModal(true)
+    }
 
+    const closeModal = () => {
+        handleModal(false)
+    }
 
+    const settingPassword = (pass) => {
+        setPassword(pass)
+    }
+
+    const body = (
+        <div style={modalStyle} className={classesModal.paper}>
+            <h2 id="simple-modal-title">{modalContent.title}</h2>
+            <p id="simple-modal-description">
+                {modalContent.content}
+            </p>
+            {
+                modalContent.ok === true ?
+                    <>
+                    {
+                        modalContent.title === 'ÉXITO' ?
+                        <button onClick={() => closeModal()}>OK</button>   
+                        :
+                        <>
+                        <input type="password" value={password} onChange={(e) => settingPassword(e.target.value)} /> 
+                        <button onClick={() => convertExcelToJson()}>Enviar</button>
+                        </>
+                    }   
+                    </>
+                    :
+                    <>
+                    <button onClick={() => closeModal()}>OK</button>
+                    </>
+            }
+            
+        </div>
+    );
 
     const convertExcelToJson = () => {
         readXlsxFile(uploadedFile).then(async (rows) => {
             console.log('rows: ', rows)
             setInventario(rows)
-            await axios.post('http://localhost:3001/products/create', {
-                data: rows,
-              })
+            try {
+                await axios.post('http://localhost:3001/products/create', {
+                    data: rows,
+                    password
+                })
+                // Abrir modal de exito
+                const inventarioJson = await axios.get('http://localhost:3001/products')
+                console.log('inventarioJson: ', inventarioJson.data)
+                setMockData(inventarioJson.data.products)
+                setModalContent({
+                    title: 'éxito'.toUpperCase(),
+                    content: '',
+                    ok: true
+                })
+            } catch (e) {
+                console.log('DEBUG #1: ', e, e.response)
+                if (e.response.data.msg === 'Contraseña incorrecta') {
+                    // Abrir modal de error
+                    setModalContent({
+                        title: 'Contraseña incorrecta'.toUpperCase(),
+                        content: 'Por favor verifique su contraseña',
+                        ok: false
+                    })
+                }
+            }
           })
     }
 
     useEffect(() => {
         (async() => {
             const inventarioJson = await axios.get('http://localhost:3001/products')
-            console.log('inventarioJson: ', inventarioJson)
-            setMockData(inventarioJson.data.data)
+            console.log('inventarioJson: ', inventarioJson.data)
+            setMockData(inventarioJson.data.products)
         })()
     }, [])
 
@@ -33,11 +128,30 @@ const Inventario = () => {
         setUploadedFile(file)
     }
 
+    const initModal = () => {
+        setPassword('')
+        setModalContent({
+            title: 'Contraseña',
+            content: 'Por favor ingrese contraseña',
+            ok: true
+        })
+        openModal()
+    }
+
     return (
         <div>
             <input type="file" onChange={(e) => loadFile(e.target.files[0])}/>
-            <button onClick={() => convertExcelToJson()}>Cargar inventario</button>
+            <button onClick={() => initModal()}>Cargar inventario</button>
             <CsvDownload data={mockData} filename='Inventario.csv'>Descarga inventario</CsvDownload>
+        
+            <Modal
+            open={modal}
+            onClose={() => handleModal()}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+            >
+                {body}
+            </Modal>
         </div>
     )
 }
